1f1f59d6e92001fa7332b4b3fe358e083ee5eed7
Léo DUBOIN <leo@duboin.com>
2022-10-26 00:29:01 +0200

    hooks: Add clang-tidy

---
+++ hooks/clang_tidy.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+from typing import List
+from commands import SyntaxAnalyzer
+from error import CommandError
+
+parser = argparse.ArgumentParser(
+        description="Check C/C++ source files' syntax against "
+                    "the current project clang-tidy configuration."
+    )
+
+parser.add_argument('--apply-fixes', action=argparse.BooleanOptionalAction,
+                    dest='fix', help='Apply fixes whenever possible')
+
+parser.add_argument('--verbose', action=argparse.BooleanOptionalAction,
+                    dest='verbose', help='Show clang-format output')
+
+parser.add_argument('files', metavar='File', nargs='+',
+                    help='The files to check against the configuration')
+
+
+class ClangTidy(SyntaxAnalyzer):
+    def __init__(self) -> None:
+        super().__init__("clang-tidy", parser)
+
+    def parse_args(self, args: List[str]) -> None:
+        super().parse_args(args)
+        if self.args.fix:
+            self.apply_fixes('--fix-error')
+        self.set_verbose(self.args.verbose)
+
+    def __call__(self) -> bool:
+        if self.args is None:
+            CommandError.InvalidOperation(self.command,
+                                          "Args not parsed before execution")
+        args = ["--quiet"]
+        return super().__call__(self.args.files, args)
+
+
+def main(argv: List[str] = sys.argv) -> bool:
+    clang_tidy = ClangTidy()
+    clang_tidy.check_installed()
+    clang_tidy.parse_args(argv[1:])
+    return clang_tidy()
+
+
+if __name__ == "__main__":
+    if not main():
+        sys.exit(1)
