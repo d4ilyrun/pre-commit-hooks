e2b80cdd9838da1a74c3bb388e725e64ab4c6136
Léo DUBOIN <leo@duboin.com>
2022-10-26 00:55:35 +0200

    v1.0.0: Add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="pre-commit-hooks",
+    version="1.0.0",
+    description="A collection of my most used pre-commit hooks",
+
+    author="Léo DUBOIN",
+    author_email="leo@duboin.com",
+)
