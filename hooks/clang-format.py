80d5648230aa4db3d0ec439fb823f5fe46829793
Léo DUBOIN <leo@duboin.com>
2022-10-25 23:21:39 +0200

    hooks: Add clang-format

---
+++ hooks/clang-format.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+from typing import List
+from commands import FormattingCommand
+from error import CommandError
+
+parser = argparse.ArgumentParser(
+        description="Check C/C++ source files' formatting against "
+                    "the current project clang-format configuration."
+    )
+
+parser.add_argument('--apply-fixes', action=argparse.BooleanOptionalAction,
+                    dest='fix', help='Apply fixes whenever possible')
+
+parser.add_argument('--verbose', action=argparse.BooleanOptionalAction,
+                    dest='verbose', help='Show clang-format output')
+
+parser.add_argument('files', metavar='File', nargs='+',
+                    help='The files to check against the configuration')
+
+
+class ClangFormat(FormattingCommand):
+    def __init__(self) -> None:
+        super().__init__("clang-format", parser)
+        self.require_diff(True)
+
+    def parse_args(self, args: List[str]) -> None:
+        super().parse_args(args)
+        if self.args.fix:
+            self.apply_fixes('-i')
+        self.set_verbose(self.args.verbose)
+
+    def __call__(self) -> bool:
+        if self.args is None:
+            CommandError.InvalidOperation(self.command,
+                                          "Args not parsed before execution")
+        args = ["--style=file"]
+        return super().__call__(self.args.files, args)
+
+
+def main(argv: List[str] = sys.argv) -> bool:
+    clang_format = ClangFormat()
+    clang_format.parse_args(argv[1:])
+    return clang_format()
+
+
+if __name__ == "__main__":
+    if not main():
+        sys.exit(1)
---
+++ hooks/commands.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import sys
+import shutil
+import subprocess as sp
+from argparse import ArgumentParser
+from typing import List
+
+from error import Error, CommandError
+
+
+class Command:
+    """ Super class for all commands """
+    def __init__(self, command: str, arg_parser: ArgumentParser) -> None:
+        self.command = command
+        self.arg_parser = arg_parser
+        self.args = None
+        self.verbose = False
+
+    def check_installed(self):
+        """ Ensure that a command is installed """
+        if shutil.which(self.command) is None:
+            CommandError.NotFound(self.command)
+
+    def parse_args(self, args: List[str]):
+        """ Parse command arguments """
+        if self.args is not None:
+            return
+        self.args = self.arg_parser.parse_args(args)
+
+    def set_verbose(self, verbose: bool) -> None:
+        self.verbose = verbose
+
+
+class FormattingCommand(Command):
+
+    """ Commands responsible for formatting files """
+    def __init__(self, command: str, arg_parser: ArgumentParser) -> None:
+        super().__init__(command, arg_parser)
+        self.fix = None
+        self.sucess = True
+        self.diff = False
+
+    def __call__(self, files: List[str], args: List[str]) -> bool:
+        """
+            Run the command on the list of files with the given arguments.
+            Return wether it was successful
+        """
+        lines = ""
+        # Add fix flag if set
+        if self.fix:
+            args.append(self.fix)
+        # Check format for each file
+        for file in files:
+            cmd_args = [self.command, *args, file]
+            if self.verbose:
+                print("running: ", cmd_args)
+
+            # Save file to compare later
+            if self.diff:
+                lines = self.__save_file(file)
+            # Run formatting
+            cmd = sp.run(cmd_args, stdout=sp.PIPE, stderr=sp.PIPE)
+            # Compare the content before/after
+            if self.diff and cmd.stdout.decode('ascii') != lines:
+                cmd.returncode = 1
+                cmd.stderr = '''
+                    Output doesn't match the original content of the file\n
+                '''.encode()
+
+            if cmd.returncode != 0:
+                Error(cmd.returncode, "{}: wrong format".format(file)).show()
+                if self.verbose:
+                    sys.stderr.write(cmd.stderr.decode('utf-8'))
+                self.sucess = False
+        return self.sucess
+
+    def apply_fixes(self, flag: str):
+        """ Wether to apply fixes whenever possible """
+        self.fix = flag
+
+    def require_diff(self, require: bool):
+        """
+            Wether we need to compare before/after formatting manually.
+            Needed whend have no simple way of detecting errors otherwise.
+            Example: clang-format
+        """
+        self.diff = require
+
+    # PRIVATE FUNCTIONS
+
+    def __save_file(self, file: str) -> str:
+        """ Save the content of a file """
+        with open(file) as f:
+            return f.read()
